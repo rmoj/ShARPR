fd3b508ebe4aa623c0f5bae2e339594ce0e88771
Parth Patel <pvpprod89@gmail.com>
2018-11-06 20:10:04 -0500

    show all cryptocurrency data in form of card

--- src/components/Tickers.js
+++ src/components/Tickers.js
@@ -14,6 +14,7 @@ class Tickers extends Component {
         //   wanted.includes(currency.id)
         // );
         // 
+        // 
         const result = response.data;
         console.log(result);
         this.setState({ data: result });
