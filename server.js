daf07c82fdc0b5e682e461d0ddfd8072afda0037
Anthony Yin <anthony923x@gmail.com>
2018-11-20 12:38:55 -0500

    setting up a express server for React

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+//console.log that server is up and running
+
+app.listen(port, () => console.log('Listening on port ${port}'));
+
+//create a GET route
+
+app.get('/express_backend', (req, res) => {
+  res.send({ express: 'Your Express Backend is Connected to React' });
+});
--- src/App.js
+++ src/App.js
@@ -2,14 +2,42 @@ import React, { Component } from "react";
 import "./App.css";
 import Tickersfew from "./components/Tickersfew";
 import CurrencyGroupPair from "./components/currencyConverter";
+import { isNullOrUndefined } from "util";
 
 class App extends Component {
+state={
+  data:null
+};
+
+componentDidMount(){
+  //call fetch function below once the component mount
+  this.callBackendAPI().then(res=>this.setState({data:res.express})).catch(err=>console.log(err))
+}
+
+// Fetches our GET route from the Express Server.
+
+callBackendAPI = async()=>{
+  const response = await fetch('/express_backend');
+  const body = await response.json()
+
+  if(response.status !==200){
+    throw Error(body.message)
+  }
+  return body
+}
+
+
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img className="App-logo" src="./SHARPR.png" alt="SHARPR LOGO" />
         </div>
+        <header>
+          // Render the newly fetched data inside of this.state.data
+          <p className="App-intro">{this.state.data}</p>
+        </header>
         <Tickersfew />
         <CurrencyGroupPair />
       </div>
