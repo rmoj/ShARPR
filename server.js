bd6dc399f7a55fdb47d3b710b033dce65649725c
Anthony Yin <anthony923x@gmail.com>
2018-11-22 18:46:59 -0500

    ready for deploying to heroku with staging and production environments

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -6,8 +7,9 @@ const port = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'client/build'));
 }
 
 //create a GET route
