080d8c645f7a3815ad391845642ce3e106c8f9f6
Anthony Yin <anthony923x@gmail.com>
2018-11-24 14:55:13 -0500

    signin UI on the top

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
+import SignInForm from './SignInUiPage/SignInForm';
+import SignUpForm from './SignInUiPage/SignUpForm';
 import './App.css';
+import './SignInUiPage/AppSignUi.css';
+import { HashRouter as Router, Route, Link, NavLink } from 'react-router-dom';
+
 import Tickersfew from './components/Tickersfew';
 import CurrencyGroupPair from './components/currencyConverter';
 
@@ -20,6 +25,54 @@ class App extends Component {
         <div className="App-header">
           <img className="App-logo" src="./SHARPR.png" alt="SHARPR LOGO" />
         </div>
+
+        <Router>
+          <div className="AppUi">
+            <div className="App__Aside" />
+            <div className="App__Form">
+              <div className="PageSwitcher">
+                <NavLink
+                  to="/sign-in"
+                  activeClassName="PageSwitcher__Item--Active"
+                  className="PageSwitcher__Item"
+                >
+                  Sign In
+                </NavLink>
+                <NavLink
+                  exact
+                  to="/"
+                  activeClassName="PageSwitcher__Item--Active"
+                  className="PageSwitcher__Item"
+                >
+                  Sign Up
+                </NavLink>
+              </div>
+
+              <div className="FormTitle">
+                <NavLink
+                  to="/sign-in"
+                  activeClassName="FormTitle__Link--Active"
+                  className="FormTitle__Link"
+                >
+                  Sign In
+                </NavLink>{' '}
+                or{' '}
+                <NavLink
+                  exact
+                  to="/"
+                  activeClassName="FormTitle__Link--Active"
+                  className="FormTitle__Link"
+                >
+                  Sign Up
+                </NavLink>
+              </div>
+
+              <Route exact path="/" component={SignUpForm} />
+              <Route path="/sign-in" component={SignInForm} />
+            </div>
+          </div>
+        </Router>
+
         <Tickersfew />
         <CurrencyGroupPair />
         <Container style={{ padding: '2em 0em' }}>
---
+++ src/SignInUiPage/signInUiApp.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { HashRouter as Router, Route, Link, NavLink } from 'react-router-dom';
+import SignUpForm from './SignInForm';
+import SignInForm from './SignUpForm';
+import './AppSignUi.css';
+
+// import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="AppUi">
+          <div className="App__Aside" />
+          <div className="App__Form">
+            <div className="PageSwitcher">
+              <NavLink
+                to="/sign-in"
+                activeClassName="PageSwitcher__Item--Active"
+                className="PageSwitcher__Item"
+              >
+                Sign In
+              </NavLink>
+              <NavLink
+                exact
+                to="/"
+                activeClassName="PageSwitcher__Item--Active"
+                className="PageSwitcher__Item"
+              >
+                Sign Up
+              </NavLink>
+            </div>
+
+            <div className="FormTitle">
+              <NavLink
+                to="/sign-in"
+                activeClassName="FormTitle__Link--Active"
+                className="FormTitle__Link"
+              >
+                Sign In
+              </NavLink>{' '}
+              or{' '}
+              <NavLink
+                exact
+                to="/"
+                activeClassName="FormTitle__Link--Active"
+                className="FormTitle__Link"
+              >
+                Sign Up
+              </NavLink>
+            </div>
+
+            <Route exact path="/" component={SignUpForm} />
+            <Route path="/sign-in" component={SignInForm} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
