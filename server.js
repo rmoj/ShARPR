ae3df266c2e72262cb0745e4d8c18ce5fcbae43e
syin <shaomingyinut@gmail.com>
2018-11-24 11:36:35 -0500

    update package.json file to run both server and client concurrently

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const jsonServer = require('json-server');
 const server = express();
 const path = require('path');
 const router = jsonServer.router(path.join(__dirname, 'coins.json'));
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3001;
 
 server.use('/api/v1', router);
 
-server.use(express.static(path.join(__dirname, 'public/')));
+server.use(express.static(path.join(__dirname, './public/')));
 
 server.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + 'public/index.html'));
+  res.sendFile(path.join(__dirname + './public/index.html'));
 });
 
 server.listen(PORT, function() {
